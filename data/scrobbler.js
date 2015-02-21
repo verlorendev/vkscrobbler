97ccf9effa0b879172bfbc34cc90010abe40c5d0
Your Name Here <verloren@ukr.net>
2015-02-21 02:32:09 +0200

    emit last played sonf between tabs

--- data/scrobbler.js
+++ data/scrobbler.js
@@ -1,30 +1,48 @@
-
-var lastSong;
+var	emitSong={name:"", author:""}; // name - 6, author -  5 - object ids
 var watchLastSong = function(){
-	lastSong = unsafeWindow.audioPlayer && unsafeWindow.audioPlayer.lastSong
-	console.log('lastSong', interval, lastSong)
-};
+		var lastSong = unsafeWindow.audioPlayer && unsafeWindow.audioPlayer.lastSong;
 
-// var watchLastSong = function(){
-// 	unsafeWindow.audioPlayer && 
-// 		watch(unsafeWindow.audioPlayer, 'lastSong', function(prop, action, newvalue, oldvalue){
-// 		console.log('Song changed', prop, action, newvalue)
-// 	});
-// };
+		if(lastSong){
+			var comparedSong = setSong({}, lastSong);
+			if(checkSongChange(emitSong, comparedSong)){
+		 		emitSong = setSong(emitSong, lastSong);
+		 		    
+				console.log('new lastSong', interval, emitSong);
+				self.port.emit("setSong", emitSong);
+			}
+		}
+	},
+	setSong = function(song, lastSong){
+		song['name'] =  lastSong[6];
+	 	song['author'] = lastSong[5];
+	 	return song;
+	},
+	copySong = function(){
+
+	},
+	checkSongChange = function(oldSong, newSong){
+
+	//	return oldSong.name!=newSong.name || oldSong.author!=newSong.author;
+		return ["name", "author"].every(function(el){
+			return newSong[el] !== oldSong[el];
+		})
+	};
 
 
-// var watchAudioPlayer = function(){
-// 	unsafeWindow.watch('audioPlayer', function(id, oldValue, newValue){
-// 		console.log('audioPlayer is watched');
-// 		if(newValue!==undefined){
-// 			console.log('newValue', newValue)
-// 			unsafeWindow.setTimeout(watchLastSong, 3000)
-// 			unsafeWindow.unwatch('audioPlayer');
-// 		};
-// 	});
-// };
 
+var interval;
 
+var setWatchInterval = function(){
+	interval = window.setInterval(watchLastSong, 3000)
+}
 
-var interval = window.setInterval(watchLastSong, 3000)
+// self.port.on("stopSong", function(song) {
+// 	// if(checkSongChange(emitSong, song))
+// 	// 	clearWatchInterval();
+// 	// else
+// 	console.log('gotSong',interval,  song);
 
+  
+// });
+
+setWatchInterval();
